df6f38df64356425cccfa39ae524de85e6bd2698
Subham Jena <subhamjena4519@gmail.com>
2023-09-01 21:30:08 +0530

    App Version 1

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+  /* ============NAV BAR POP UP STARTS FORM HERE======================   */
+  // const tripType = document.getElementsByName("tripSide");
+  // const radioType =()=>{
+  //   let selected = document.querySelector("input[name='tripSide']:checked").value;
+  //   console.log(selected);
+  // }
+
+  // radioType();
+
+  // // for (let i = 0; i <3; i++) {
+  // //   // console.log(tripType[i])
+  // // }
+  // tripType.forEach(radio => { 
+  //   console.log("hii");
+  //   radio.addEventListener("change",radioType)
+  // })
